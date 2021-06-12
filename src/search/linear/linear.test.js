d353ccb8a0cb3bb42d53abb354b9316b7fa747d1
Viktor Ulyankin <freeman.viktor@mail.ru>
2021-06-12 15:10:56 +0300

    Small edits on test.

--- src/search/linear/linear.test.js
+++ src/search/linear/linear.test.js
@@ -5,14 +5,15 @@ describe.each([
   [[0, 1, 2, 3], 2, 2],
   [[3, 2, 1, 0], 2, 1],
   [[3, 2, 1, 0], 0, 3],
+  [[3, 2, -1, 1, 0], 1, 3],
   [[0, 1000000000, 2000000000, 2, 3000000000], 2, 3],
-  [Array(1000000).fill(0).map((item, i, arr) => arr.length - i), 2, 999998],
+  [Array(1000000).fill(0).map((item, i, arr) => arr.length - i), 450002, 549998],
   [[3, 2, 1, 0], 100, -1],
   [[], 1, -1],
   [[''], 1, -1],
   [['a', 'bb', 'ccc', 'dddd'], 'dddd', 3],
 
-])('Test linearSearch() on tests[%#]', (array, search, expected) => {
+])('Test linearSearch() [%#]', (array, search, expected) => {
   test('Should return correct value', () => {
     expect(linearSearch(array, search)).toBe(expected);
   });
