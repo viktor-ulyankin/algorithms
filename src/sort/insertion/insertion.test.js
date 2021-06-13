80cc060e0af893ef01ae4f4d7d3cfb3ffc88f109
Viktor Ulyankin <freeman.viktor@mail.ru>
2021-06-13 16:38:35 +0300

    insertionSort() (Edit test).

--- src/sort/insertion/insertion.test.js
+++ src/sort/insertion/insertion.test.js
@@ -6,10 +6,10 @@ describe.each([
   [[3, 2, 1, 0], [0, 1, 2, 3]],
   [[11, 0, -1, -2, 3], [-2, -1, 0, 3, 11]],
   [[70000000000, 10, -70000000000, -50000000000], [-70000000000, -50000000000, 10, 70000000000]],
-  // [
-  //   Array(100).fill(0).map((item, i, array) => array.length - i),
-  //   Array(100).fill(0).map((item, i) => i + 1),
-  // ],
+  [
+    Array(100).fill(0).map((item, i, array) => array.length - i),
+    Array(100).fill(0).map((item, i) => i + 1),
+  ],
   [[], []],
 
 ])('Test insertionSort() [%#]', (array, expected) => {
